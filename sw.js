817b9c4c6ac91d3abdf5fe3c810b94c465919a0e
chesteric31 <chesteric31@gmail.com>
2017-05-30 22:25:37 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,68 @@
+var cacheName = 'pwa-sers';
+
+var filesToCache = [
+    './',
+    './css/style.css',
+    './images/icons/icon-72x72.png',
+    './images/icons/icon-96x96.png',
+    './images/icons/icon-128x128.png',
+    './images/icons/icon-144x144.png',
+    './images/icons/icon-152x152.png',
+    './images/icons/icon-192x192.png',
+    './images/icons/icon-384x384.png',
+    './images/icons/icon-512x512.png',
+    './js/app.js',
+    './js/menu.js',
+    './js/offline.js',
+    './js/toast.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+
+    console.log('Service Worker: Installing....');
+
+    event.waitUntil(
+
+        // Open the Cache
+        caches.open(cacheName).then(function(cache) {
+            console.log('Service Worker: Caching App Shell at the moment......');
+
+            // Add Files to the Cache
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+// Fired when the Service Worker starts up
+self.addEventListener('activate', function(event) {
+
+    console.log('Service Worker: Activating....');
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(key) {
+                if( key !== cacheName) {
+                    console.log('Service Worker: Removing Old Cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+    console.log('Service Worker: Fetch', event.request.url);
+
+    console.log("Url", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
