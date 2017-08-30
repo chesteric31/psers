dfd237ebef7530199cd13bac6741f35716d811c2
eric <chesteric31@gmail.com>
2017-08-30 09:27:15 +0200

    cache index.html

--- sw.js
+++ sw.js
@@ -3,11 +3,20 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/sw-toolbox/3.6.1/sw-toolbo
 toolbox.precache(['index.html', 'latest.html', './css/style.css', './images/push-off.png', './images/push-on.png']);
 toolbox.router.get('/images*', toolbox.cacheFirst);
 
-toolbox.router.get('/', toolbox.cacheFirst, {
+/*toolbox.router.get('/', toolbox.cacheFirst, {
     cache: {
         name: 'pwa-sers-cache',
     }
-})
+})*/
+self.toolbox.router.get('/(.*)', function(req, vals, opts) {
+  return toolbox.networkFirst(req, vals, opts)
+    .catch(function(error) {
+      if (req.method === 'GET' && req.headers.get('accept').includes('text/html')) {
+        return toolbox.cacheOnly(new Request('/index.html'), vals, opts);
+      }
+      throw error;
+    });
+});
 
 self.addEventListener('push', function(event) {
 
